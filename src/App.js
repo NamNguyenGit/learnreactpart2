8a1cf7e17b8947965edbd720caa08627ae614cb4
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2021-12-20 14:02:20 +0700

    Test call api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
-import "./App.css";
-import NotFound from "./Components/NotFound";
-import Footer from "./Features/Header-Footer/Footer";
-import Header from "./Features/Header-Footer/Header";
-import ProductFeatures from "./Features/Product";
-import TodoFeatures from "./Features/Todo";
+import { useEffect } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import productApi from './api/productApi';
+import './App.css';
+import NotFound from './Components/NotFound';
+import Footer from './Features/Header-Footer/Footer';
+import Header from './Features/Header-Footer/Header';
+import ProductFeatures from './Features/Product';
+import TodoFeatures from './Features/Todo';
 
 function App() {
+  useEffect(() => {
+    const params = {
+      id:  13761499,
+    };
+    const fetchProduct = async () => {
+      const productlist = await productApi.getAll(params);
+      console.log(productlist);
+    };
+
+    fetchProduct();
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
